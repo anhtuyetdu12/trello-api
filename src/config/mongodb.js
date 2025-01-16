c499ac70319b3e0cd902f67205f87dcc1f8e55d2
ahtwet12 <anhtuyetdu21@gmail.com>
2025-01-16 23:11:22 +0700

    connect_mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,25 @@
 /**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
+ * Updated by anhtuyetdev.com's author on August 17 2023
+ * YouTube: https://youtube.com/@anhtuyetdev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+//twettanh12
+//UXyVPeCkmPrxKXQx
+
+const MONGODB_URI = 'mongodb+srv://twettanh12:UXyVPeCkmPrxKXQx@cluster0-anhtuyetdev.3mord.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0-AnhTuyetDev'
+
+const DATABASE_NAME = 'trello-anhtuyetdev-mern-stack-pro'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+let trelloDatabaseInstance = null
+
+//khoi tao 1 doi tuong mongoClientInstance de connect toi MongoDB
+const clientInstance = new MongoClient(MONGODB_URI, {
+  serverApi:{
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
