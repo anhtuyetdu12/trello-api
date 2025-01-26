945fb3a585638e81fe846d156e1770f29c9c366c
ahtwet12 <anhtuyetdu21@gmail.com>
2025-01-26 09:45:35 +0700

    delete_column_and_card

--- src/validations/columnValidation.js
+++ src/validations/columnValidation.js
@@ -51,8 +51,23 @@ const update = async(req, res, next) => {
   }
 }
 
+const deleteItem = async(req, res, next) => {
+  const correctCodition = Joi.object({
+    id: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+  })
+  try {
+    await correctCodition.validateAsync(req.params)
+    next()
+  } catch (error) {
+    const errorMessage = new Error(error).message
+    const customError = new ApiError(StatusCodes.UNPROCESSABLE_ENTITY, errorMessage)
+    next(customError)
+
+  }
+}
 
 export const columnValidation = {
   createNew,
-  update
+  update,
+  deleteItem
 }
